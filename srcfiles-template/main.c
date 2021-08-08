6ac88dd595bd872df0483cb9649c8b88035b8b67
MJ <56290324+mjneri@users.noreply.github.com>
2021-08-08 18:03:03 +0800

    Source files template

---
+++ srcfiles-template/main.c
@@ -0,0 +1,37 @@
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+// main.c -- Main C Firmware code
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+// Author: mjneri
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+//
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+// Module Name: main.c
+// Description: 
+//
+// Revisions:
+// Revision 0.01 - File Created (DATE HERE)
+// Additional Comments:
+//
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+// Include header files here
+// ...
+
+// Global Variables
+// Define variables that will be accessed by ISRs as 'volatile'
+// ...
+
+// Extern Variables
+// ...
+
+int main(void)
+{
+    // Initialize all modules
+    // ...
+
+    while(1)
+    {
+        // Write application code here
+        // ...
+    }
+}
---
+++ srcfiles-template/system.h
@@ -0,0 +1,36 @@
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+// system.h -- Header file for system.c
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+// Author: mjneri
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+//
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+// Module Name: system.h
+// Description:
+//
+// Revisions:
+// Revision 0.01 - File Created (DATE HERE)
+// Additional Comments:
+//
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+// Header Guards
+#ifndef _SYSTEM_H_
+#define _SYSTEM_H_
+
+// Project File includes (like stdio.h, etc.)
+// ...
+
+// Other header files
+// ...
+
+// Define Macros
+// ...
+
+// Define System level constants
+// ...
+
+// Function prototypes
+// ...
+
+#endif /*_SYSTEM_H_*/
